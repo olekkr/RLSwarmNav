e50ced19cd81eea6fc8445a6f1fead29983fcd05
olekkr <olekkr@outlook.com>
2025-11-26 11:20:06 +0100

    crazyflie testing

---
+++ code/flietest.py
@@ -0,0 +1,21 @@
+import time
+import logging 
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.utils import uri_helper
+
+def simple_connect():
+    print("Yeah, I'm connected! :D")
+    time.sleep(3)
+    print("Now I will disconnect :'(")
+
+
+# URI to the Crazyflie to connect to
+uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+if __name__ == '__main__':
+    # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
+    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        simple_connect()
