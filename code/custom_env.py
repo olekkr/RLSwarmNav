wrong sign on penalty

code/custom_env.py
@@ -130,7 +130,7 @@ class CustomAviary(BaseRLAviary):
                 # penalty for getting near other drones 
                 # ret -= 150 * max(0, 0.02 - np.linalg.norm(states[i][0:3]- states[ii][0:3]) **4)
                 # Simpler version: 
-                ret += max(0, 2 - np.linalg.norm(states[ii][0:3]-states[i][0:3])**4)
+                ret -= max(0, 2 - np.linalg.norm(states[ii][0:3]-states[i][0:3])**4)
                 # ret -= 2.5 if np.linalg.norm(states[i][0:3]- states[ii][0:3]) < 1 else 0 
                 pass
 